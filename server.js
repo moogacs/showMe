4a9c71214f5d3747c406f57a9e6c2ca1d0a2b98f
= <contact@m-mooga.com>
2019-09-02 21:11:04 +0200

    step 1 for view 1

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ let express = require("express")
 app = express()
 port = process.env.PORT || 3000;
 
+// GET method route
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+})
+
+  // POST method route
+app.post('/register', function (req, res) {
+    console.log(req)
+    res.send('Trying to add user')
+})
 
 app.listen(port)
 console.log("showMe listening on "+ port)
